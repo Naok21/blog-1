4455c7df21fd0f776a3c2c8265bfab9d7db560e1
Naok21 <nassimkridel@hotmail.fr>
2021-11-15 17:13:14 +0100

    update 10/11/21

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
